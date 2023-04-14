a5da49ab7b8eb1ae4acdb4f4b1c533379b5e1a79
ciglesiasgastaldi <ciglesiasgastaldi@gmail.com>
2023-04-14 16:51:44 -0300

    snippet imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch, parallel} = require("gulp");
+
+//CSS
 const  sass = require("gulp-sass")(require('sass')); //importa
 const  plumber = require('gulp-plumber');
+
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const  webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
 
 function css(done) {
     src("src/scss/**/*.scss")//1. identificar el archivo de SASS: src
@@ -12,6 +21,39 @@ function css(done) {
     done(); //callback que avisa a gulp cuando llega al final la ejecución de la fx
 }
 
+function imagenes( done ) {
+    const  opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin(opciones)))
+    .pipe( dest('build/img'))
+
+    done();
+
+}
+
+function versionWebp( done ){
+    const  opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe( webp(opciones) )
+    .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif( done ){
+    const  opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+    .pipe( avif(opciones) )
+    .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css)
 
@@ -20,4 +62,7 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, dev);
